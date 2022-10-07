1577bbcbc372ecea9a910aaf01453c9c1f69bc1a
Max_S <maxi.sm117@gmail.com>
2022-10-07 17:45:23 -0300

    :zap: Adding server features

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const { register, login, setAvatar } = require("../controllers/usersController");
+
+const router = require("express").Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/setAvatar/:id", setAvatar)
+
+module.exports = router;
